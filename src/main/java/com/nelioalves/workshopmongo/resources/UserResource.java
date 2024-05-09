8f3f328c160f9426d476322cafac3fac722c3bb5
Eric Avelino <ericfravelino@gmail.com>
2024-05-09 17:13:42 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.nelioalves.workshopmongo.resources;
 
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -24,9 +26,10 @@ public class UserResource {
     //ResponseEntity ja retorna os possiveis cabecalhos
     //formatadinhos
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
         //Vai retornar um ResponseEntity do tipo , e estamos usando
         //o metodo ok(), que vai instanciar o objeto ResponseEntity
         //ja com o codigo de resposta http de forma que a resposta
