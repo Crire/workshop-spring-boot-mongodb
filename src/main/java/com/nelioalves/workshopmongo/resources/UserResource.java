cb0a3e15a1e7c08c7e779c87ce59a418ced64a0a
Eric Avelino <ericfravelino@gmail.com>
2024-05-09 16:55:49 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.nelioalves.workshopmongo.resources;
 
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,14 +17,15 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
+
     //ResponseEntity ja retorna os possiveis cabecalhos
     //formatadinhos
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> list = new ArrayList<User>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
         //Vai retornar um ResponseEntity do tipo , e estamos usando
         //o metodo ok(), que vai instanciar o objeto ResponseEntity
